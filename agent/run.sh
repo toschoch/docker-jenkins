b86ded1d85e11c70c5e87109b54190974cb36e71
Tobias Schoch <tobias.schoch@helbling.ch>
2018-11-28 12:32:26 +0100

    agent based on pure alpine image so arch are automatically supported

--- agent/run.sh
+++ agent/run.sh
@@ -1,22 +1,16 @@
 #! /bin/bash -e
 
 echo "looking for secrets..."
-echo "... /run/secrets/${JENKINS_USER}"
 if [ -f "/run/secrets/${JENKINS_USER}" ]; then
     echo "found '${JENKINS_USER}' secret!"
     read JENKINS_USER < /run/secrets/${JENKINS_USER}
     export JENKINS_USER 
-else
-    echo "secret '${JENKINS_USER}' not found!"
 fi
 
-echo "... /run/secrets/${JENKINS_PASS}"
 if [ -f "/run/secrets/${JENKINS_PASS}" ]; then
     echo "found '${JENKINS_PASS}' secret!"
     read JENKINS_PASS < /run/secrets/${JENKINS_PASS}
     export JENKINS_PASS
-else
-    echo "secret '${JENKINS_PASS}' not found!"
 fi
 
 # GROUP=$(stat -c %G /var/run/docker.sock)
