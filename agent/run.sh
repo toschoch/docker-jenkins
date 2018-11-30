6d7ab4a3abcccb7dc1fac31444dc827008c170f2
Tobias Schoch <tobias.schoch@helbling.ch>
2018-11-30 18:34:18 +0100

    working jenkins master and swarm plugin agents

--- agent/run.sh
+++ agent/run.sh
@@ -3,15 +3,22 @@
 echo "looking for secrets..."
 if [ -f "/run/secrets/${JENKINS_USER}" ]; then
     echo "found '${JENKINS_USER}' secret!"
-    read JENKINS_USER < /run/secrets/${JENKINS_USER}
-    export JENKINS_USER 
+    read USR < /run/secrets/${JENKINS_USER}    
+else
+    USR=${JENKINS_USER}
 fi
+COMMAND_OPTIONS="${COMMAND_OPTIONS} -username $USR"
 
 if [ -f "/run/secrets/${JENKINS_PASS}" ]; then
     echo "found '${JENKINS_PASS}' secret!"
-    read JENKINS_PASS < /run/secrets/${JENKINS_PASS}
-    export JENKINS_PASS
+    read PSS < /run/secrets/${JENKINS_PASS}
+    export PSS
+else
+    PSS=${JENKINS_PASS}
+    export PSS
 fi
+COMMAND_OPTIONS="${COMMAND_OPTIONS} -passwordEnvVariable PSS"
+COMMAND_OPTIONS="${COMMAND_OPTIONS} -name $(hostname) -disableClientsUniqueId"
 
 # GROUP=$(stat -c %G /var/run/docker.sock)
 # if [ ! $(grep ^$GROUP: /etc/group) ]; then
@@ -19,5 +26,5 @@ fi
 # fi
 # adduser jenkins $GROUP
 
-# As argument is not jenkins, assume user want to run his own process, for example a `bash` shell to explore this image
-exec "$@"
+# exec su jenkins -c "exec java -jar /home/jenkins/swarm-client-${SWARM_CLIENT_VERSION}.jar ${COMMAND_OPTIONS}"
+java -jar /home/jenkins/swarm-client-${SWARM_CLIENT_VERSION}.jar ${COMMAND_OPTIONS}
