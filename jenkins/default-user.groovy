b35b1651b95082d966ada5c3cb07640632788cb3
Tobias Schoch <tobias.schoch@helbling.ch>
2018-11-30 12:13:06 +0100

    working jenkins without blueocean

--- jenkins/default-user.groovy
+++ jenkins/default-user.groovy
@@ -3,12 +3,28 @@ import hudson.security.*
 
 def env = System.getenv()
 
+def userName = env.JENKINS_USER
+def userPass = env.JENKINS_PASS
+
+def userNameFile = new File(env.SECRETS_DIR + "/" + userName)
+def userPassFile = new File(env.SECRETS_DIR + "/" + userPass)
+
+if (userNameFile.exists())
+{
+    userName = userNameFile.text.trim()
+}
+if (userPassFile.exists())
+{
+    userPass = userPassFile.text.trim()
+}
+
+
 def jenkins = Jenkins.getInstance()
 jenkins.setSecurityRealm(new HudsonPrivateSecurityRealm(false))
 jenkins.setAuthorizationStrategy(new GlobalMatrixAuthorizationStrategy())
 
-def user = jenkins.getSecurityRealm().createAccount(env.JENKINS_USER, env.JENKINS_PASS)
+def user = jenkins.getSecurityRealm().createAccount(userName, userPass)
 user.save()
 
-jenkins.getAuthorizationStrategy().add(Jenkins.ADMINISTER, env.JENKINS_USER)
+jenkins.getAuthorizationStrategy().add(Jenkins.ADMINISTER, userName)
 jenkins.save()
