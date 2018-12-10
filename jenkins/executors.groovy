6b2c787f298d2696ef0727f4900d6baa83264e31
Tobias Schoch <Tobias.Schoch@helbling.ch>
2018-12-10 18:12:40 +0100

    removed jenkins executors for master instance and related packages

--- jenkins/executors.groovy
+++ jenkins/executors.groovy
@@ -1,12 +1,2 @@
 import jenkins.model.*
-import hudson.model.*
-
-Jenkins.instance.setNumExecutors(2)
-
-hudson = hudson.model.Hudson.instance
-hudson.slaves.each { slave -> 
-  print "Slave  $slave.nodeName : Labels: $slave.labelString"
-  slave.labelString = slave.labelString + " " + "docker"
-  println "   --> New labels: $slave.labelString"
-}
-hudson.save()
+Jenkins.instance.setNumExecutors(0)
