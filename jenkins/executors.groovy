b975570055e942a19e4ea86c6aebbb5ec50840d4
Tobias Schoch <Tobias.Schoch@helbling.ch>
2018-12-10 13:31:33 +0100

    add docker not offline to node labels

--- jenkins/executors.groovy
+++ jenkins/executors.groovy
@@ -6,7 +6,7 @@ Jenkins.instance.setNumExecutors(5)
 hudson = Hudson.instance
 hudson.slaves.findAll { it.nodeName.equals("slave4") }.each { slave -> 
   print "Slave  $slave.nodeName : Labels: $slave.labelString"
-  slave.labelString = slave.labelString + " " + "offline"
+  slave.labelString = slave.labelString + " " + "docker"
   println "   --> New labels: $slave.labelString"
 }
 hudson.save()
